bf9b98e85ba0a985dac4232abeef5487aed4845a
Asaduzzaman-Nur-Shuvo <shuvo@meldcx.com>
2025-03-10 15:00:19 +0600

    NULL assign in main() as third parameter, as it uis ununsed

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 
 int mosquitto_broker_main(int argc, char *argv[], int (*pw_callback)(char* buf, int size, int rwflag, void* userdata));
 
-int main(int argc, char *argv[], int (*pw_callback)(char* buf, int size, int rwflag, void* userdata))
+int main(int argc, char *argv[])
 {
-  return mosquitto_broker_main(argc, argv, pw_callback);
+  return mosquitto_broker_main(argc, argv, NULL);
 }
